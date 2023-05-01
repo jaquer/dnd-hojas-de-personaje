9d16eb8bb945eacd069f72db5bce96d12b411425
Arturo <jaquer@users.noreply.github.com>
2023-05-01 16:49:30 -0700

    Inital export for GH Pages.

---
+++ precache-manifest.af30cab4fe41a0a956305a05bfb91950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674129e45b57b59faf621c5d2ca61333",
+    "url": "./index.html"
+  },
+  {
+    "revision": "405c80ea91b4174eeafd",
+    "url": "./static/css/2.3d01e3ca.chunk.css"
+  },
+  {
+    "revision": "9a0df2beb3b089cf95b0",
+    "url": "./static/css/main.7eb9f2ce.chunk.css"
+  },
+  {
+    "revision": "405c80ea91b4174eeafd",
+    "url": "./static/js/2.bd8af3e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.bd8af3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0df2beb3b089cf95b0",
+    "url": "./static/js/main.8c9dfcc4.chunk.js"
+  },
+  {
+    "revision": "b6024dd17ec1640bc9db",
+    "url": "./static/js/runtime-main.f699e026.js"
+  }
+]);
